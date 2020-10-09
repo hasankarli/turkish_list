4f70f0dd6b7938f19e4e337792e4ba15fd697e41
Hasan Karlı <karlihasann@gmail.com>
2020-10-09 22:44:59 +0300

    add get city name when given a plate number

--- lib/turkish_list.rb
+++ lib/turkish_list.rb
@@ -1,10 +1,19 @@
 require "turkish_list/version"
 
 module TurkishList
-  class Car
-    def self.who_am_i
-      puts "Car class"
+
+  class Cities
+    cities = {}
+    def initialize
+      file = File.read('./cities.json')
+      cities = JSON.parse(file)
     end
+
+    def city_name_for_plate(plate_number)
+      city_name = cities.select {|k,v| k == plate_number}
+      return city_name.values
+    end 
+  
   end
   
 end
