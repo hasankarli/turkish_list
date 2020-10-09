0d58030a12fc1341b42a9326cdc76f8a0f78ae7a
Hasan Karlı <karlihasann@gmail.com>
2020-10-09 23:34:13 +0300

    bug fix

--- lib/turkish_list.rb
+++ lib/turkish_list.rb
@@ -4,7 +4,7 @@ require 'turkish_list/city'
 module TurkishList
 
   def self.city_name_by_plate_number(plate_number)
-    City.find_by_plate_number(plate_number)
+    City.new.find_by_plate_number(plate_number)
   end
   
 end
